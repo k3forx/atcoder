a0a916f429a09ff67aa893003c1f01d0dcb9f7e8
Kanata Miyahana <afiri.bupa@gmail.com>
2022-02-06 21:59:40 +0900

    this is my first solved C problem

--- beginner_contest/190/bowls_and_dishes/main.cpp
+++ beginner_contest/190/bowls_and_dishes/main.cpp
@@ -3,14 +3,70 @@ using namespace std;
 
 int main()
 {
-	int X;
-	cin >> X;
-	if (X == 7 || X == 5 || X == 3)
+	int N, M;
+	cin >> N >> M;
+
+	vector<vector<int> > cond(M, vector<int>(2));
+	for (int i = 0; i < M; i++)
 	{
-		cout << "YES" << endl;
+		cin >> cond.at(i).at(0) >> cond.at(i).at(1);
 	}
-	else
+
+	int K;
+	cin >> K;
+
+	vector<vector<int> > person(K, vector<int>(2));
+	for (int i = 0; i < K; i++)
 	{
-		cout << "NO" << endl;
+		cin >> person.at(i).at(0) >> person.at(i).at(1);
 	}
+
+	int res = 0;
+	for (int bit = 0; bit < (1 << K); bit++)
+	{
+		vector<int> select(K, 0);
+		for (int i = 0; i < K; i++)
+		{
+			// デフォルトでは左側の皿を選ぶとする
+			select.at(i) = person.at(i).at(0);
+			if (bit & (1 << i))
+			{
+				// この条件に引っかかる場合は右側の皿を選ぶとする
+				select.at(i) = person.at(i).at(1);
+			}
+		}
+
+		int tmp = 0;
+		for (int i = 0; i < M; i++)
+		{
+			bool hasLeft = false;
+			bool hasRight = false;
+
+			int d1 = cond.at(i).at(0);
+			int d2 = cond.at(i).at(1);
+
+			for (int j = 0; j < K; j++)
+			{
+				if (d1 == select.at(j))
+				{
+					hasLeft = true;
+				}
+				if (d2 == select.at(j))
+				{
+					hasRight = true;
+				}
+			}
+
+			if (hasLeft & hasRight)
+			{
+				tmp++;
+			}
+		}
+
+		if (tmp > res)
+		{
+			res = tmp;
+		}
+	}
+	cout << res << endl;
 }
