df74f1a524431ac34200b624837ed17c40a716fa
Kanata Miyahana <afiri.bupa@gmail.com>
2022-02-07 22:56:04 +0900

    solve c

--- beginner_contest/182/c_to_3/main.cpp
+++ beginner_contest/182/c_to_3/main.cpp
@@ -3,9 +3,41 @@ using namespace std;
 
 int main()
 {
-	int N;
+	string N;
 	cin >> N;
 
-	int digits = to_string(N).size();
-	cout << digits << endl;
+	int digits = N.size();
+
+	bool canDivide = false;
+	int res = digits;
+	for (int bit = 0; bit < (1 << digits); bit++)
+	{
+		string tmp = "";
+		for (int i = 0; i < digits; i++)
+		{
+			if (bit & (1 << i))
+			{
+				tmp.push_back(N.at(i));
+			}
+		}
+
+		if (tmp.size() > 0)
+		{
+			int r = digits - tmp.size();
+			if ((stol(tmp) % 3 == 0) & (r < res))
+			{
+				res = r;
+				canDivide = true;
+			}
+		}
+	}
+
+	if (canDivide)
+	{
+		cout << res << endl;
+	}
+	else
+	{
+		cout << -1 << endl;
+	}
 }
