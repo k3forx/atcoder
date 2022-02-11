71ba9f95cdf41832458287a154a563f78271a3c7
Kanata Miyahana <afiri.bupa@gmail.com>
2022-02-11 22:18:10 +0900

    solve c

--- beginner_contest/173/c_h_and_v/main.cpp
+++ beginner_contest/173/c_h_and_v/main.cpp
@@ -6,13 +6,65 @@ int main()
 	int H, W, K;
 	cin >> H >> W >> K;
 
-	vector<vector<string> > mat(H, vector<string>(W));
+	vector<vector<char> > mat(H, vector<char>(W));
 	for (int i = 0; i < H; i++)
 	{
 		for (int j = 0; j < W; j++)
 		{
 			cin >> mat.at(i).at(j);
-			cout << mat.at(i).at(j) << endl;
+			// cout << mat.at(i).at(j);
+		}
+		// cout << endl;
+	}
+
+	// cout << "-----" << endl;
+	int res = 0;
+	for (int bit = 0; bit < (1 << (H + W)); bit++)
+	{
+		int cnt = 0;
+		vector<vector<char> > tmp = mat;
+		for (int i = 0; i < (H + W); i++)
+		{
+			if (bit & (1 << i))
+			{
+				// cout << "bit: " << bit << ", i: " << i << endl;
+				// remove line
+				// if i = 0, 1, then remove rows
+				// if i = 2, 3, 4, the remove columns
+				if (i <= H - 1)
+				{
+					for (int k = 0; k < W; k++)
+					{
+						tmp.at(i).at(k) = '.';
+					}
+				}
+				else
+				{
+					for (int k = 0; k < H; k++)
+					{
+						tmp.at(k).at(i - H) = '.';
+					}
+				}
+			}
+		}
+
+		for (int i = 0; i < H; i++)
+		{
+			for (int j = 0; j < W; j++)
+			{
+				char el = tmp.at(i).at(j);
+				if (el == '#')
+				{
+					cnt++;
+				}
+			}
+		}
+
+		if (cnt == K)
+		{
+			res++;
 		}
 	}
+
+	cout << res << endl;
 }
