2841a185961c78da9d1d54d7a4b4c45aa9f3769f
Kanata Miyahana <afiri.bupa@gmail.com>
2022-02-11 23:08:47 +0900

    init c

---
+++ beginner_contest/197/c_orxor/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int N;
+	cin >> N;
+
+	vector<long long> a(N);
+	for (int i = 0; i < N; i++)
+	{
+		cin >> a.at(i);
+	}
+
+	cout << "-----" << endl;
+}
