7b8f758d5c4ddb0473c9799961cc780d37f4cc01
Kanata Miyahana <afiri.bupa@gmail.com>
2022-02-14 23:26:31 +0900

    update

--- beginner_contest/051/c_back_and_forth/main.cpp
+++ beginner_contest/051/c_back_and_forth/main.cpp
@@ -3,19 +3,6 @@ using namespace std;
 
 int main()
 {
-	string s;
-	cin >> s;
-
-	for (int i = 0; i < s.size(); i++)
-	{
-		if (s.at(i) == ',')
-		{
-			cout << ' ';
-		}
-		else
-		{
-			cout << s.at(i);
-		}
-	}
-	cout << endl;
+	int sx, sy, tx, ty;
+	cin >> sx >> sy >> tx >> ty;
 }
