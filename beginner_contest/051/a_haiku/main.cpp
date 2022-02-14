8c8fb6175ea16cdfa04e6e7bfee54b00a20cdedf
Kanata Miyahana <afiri.bupa@gmail.com>
2022-02-14 23:19:19 +0900

    solve a and b

---
+++ beginner_contest/051/a_haiku/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+
+	for (int i = 0; i < s.size(); i++)
+	{
+		if (s.at(i) == ',')
+		{
+			cout << ' ';
+		}
+		else
+		{
+			cout << s.at(i);
+		}
+	}
+	cout << endl;
+}
