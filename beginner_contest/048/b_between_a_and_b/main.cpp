c236005000579243b02cbeded9c5e942eb5ebd24
Kanata Miyahana <afiri.bupa@gmail.com>
2022-02-14 22:32:29 +0900

    solve b

--- beginner_contest/048/b_between_a_and_b/main.cpp
+++ beginner_contest/048/b_between_a_and_b/main.cpp
@@ -6,29 +6,8 @@ int main()
 	long long a, b, x;
 	cin >> a >> b >> x;
 
-	long long tmp = b;
-	for (long long i = b; i >= a; i--)
-	{
-		if (i % x == 0)
-		{
-			tmp = i;
-			break;
-		}
-	}
-
-	long long tmp1 = a;
-	for (long long i = a; i <= b; i++)
-	{
-		if (i % x == 0)
-		{
-			tmp1 = i;
-			break;
-		}
-	}
-
-	// cout << tmp / x << a / x << endl;
-	long long res = (tmp / x) - (tmp1 / x);
-	if (res > 0)
+	long long res = b / x - a / x;
+	if (a % x == 0)
 	{
 		cout << res + 1 << endl;
 	}
