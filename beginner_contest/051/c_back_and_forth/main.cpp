65f59861b921f7ec57f38ff025364389d208e6e7
Kanata Miyahana <afiri.bupa@gmail.com>
2022-02-15 22:30:48 +0900

    solve c

--- beginner_contest/051/c_back_and_forth/main.cpp
+++ beginner_contest/051/c_back_and_forth/main.cpp
@@ -5,4 +5,17 @@ int main()
 {
 	int sx, sy, tx, ty;
 	cin >> sx >> sy >> tx >> ty;
+
+	int dx = tx - sx;
+	int dy = ty - sy;
+
+	cout << string(dx, 'R') << string(dy, 'U');
+
+	cout << string(dx, 'L') << string(dy, 'D');
+
+	cout << 'D' << string(dx + 1, 'R') << string(dy + 1, 'U') << 'L';
+
+	cout << 'U' << string(dx + 1, 'L') << string(dy + 1, 'D') << 'R';
+
+	cout << endl;
 }
